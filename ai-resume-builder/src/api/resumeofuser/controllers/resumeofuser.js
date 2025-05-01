b81ff5c3388a04a348ea750a8e95c2604983de06
shreeya1-23 <shreeyakhtr@gmail.com>
2025-05-01 18:16:00 +0530

    initial commit

---
+++ ai-resume-builder/src/api/resumeofuser/controllers/resumeofuser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resumeofuser controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::resumeofuser.resumeofuser');
