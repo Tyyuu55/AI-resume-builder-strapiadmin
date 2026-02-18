7e3acb0b94ee23f57849601093f5304fcd336d5e
shreeya1-23 <shreeyakhtr@gmail.com>
2026-02-18 16:53:49 +0545

    Add Vercel frontend to CORS configuration

--- ai-resume-builder/config/middlewares.js
+++ ai-resume-builder/config/middlewares.js
@@ -1,8 +1,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'https://ai-resume-builder-trv5.vercel.app/',
+        'https://ai-resume-builder-strapiadmin.onrender.com',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
